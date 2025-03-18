eae883c3263790c6f853b0ac39290926862944df
neelarani <neela9624@gmail.com>
2025-03-18 10:54:13 +0600

    connect with client

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-import 'dotenv/config';  
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectMongoDB from './config.js';
-import userRouter from './routes/routeForUser.js'
+import userRouter from './routes/routeForUser.js';
 import cookieParser from 'cookie-parser';
 const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.use(cors({
-    credentials : true,
-    origin : ["http://localhost:5173" ,"http://localhost:5174"]
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'https://gyanflow-ca428.web.app',
+      'https://gyanflow-ca428.firebaseapp.com',
+    ],
+  })
+);
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 connectMongoDB();
 
-app.use('/gyanflow/user' , userRouter )
+app.use('/gyanflow/user', userRouter);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
