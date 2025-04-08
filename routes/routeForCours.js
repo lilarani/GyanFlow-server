b841ec1ad74de027961a3b949d81c5c8bfcb1ae6
neelarani <neela9624@gmail.com>
2025-04-08 11:03:06 +0600

    update all course api

--- routes/routeForCours.js
+++ routes/routeForCours.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { addCourse, getAllPost , courseForInstructor } from '../controllers/courseController.js';
+import {
+  addCourse,
+  getAllPost,
+  courseForInstructor,
+} from '../controllers/courseController.js';
 import verifyToken from './../middlewares/verifytoken.js';
 import verifyAdmin from './../middlewares/verifyAdmin.js';
 let router = express.Router();
 
 router.post('/add-course', verifyToken, verifyAdmin, addCourse);
-router.get('/all-course', verifyToken, verifyAdmin, getAllPost);
-router.get('/course-for-instructor/:id' , courseForInstructor)
+router.get('/all-course', verifyToken, getAllPost);
+router.get('/course-for-instructor/:id', courseForInstructor);
 export default router;
