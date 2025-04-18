003bd613cb4322b3639c3afa785c3f9f403bb8bc
neelarani <neela9624@gmail.com>
2025-04-18 19:40:32 +0600

    added announcement api

--- controllers/announcementController.js
+++ controllers/announcementController.js
@@ -1,10 +1,8 @@
-import Announcement from "../models/AnnouncementModel.js";
+import Announcement from '../models/AnnouncementModel.js';
 
 let addAnnouncement = async (req, res) => {
   try {
     let { title, description, category, date, link } = req.body;
-<<<<<<< HEAD
-=======
     console.log(req.body);
 
     let newAnnouncement = new Announcement({
@@ -16,12 +14,11 @@ let addAnnouncement = async (req, res) => {
     });
 
     await newAnnouncement.save();
-    console.log("Announcement Add Done");
+    console.log('Announcement Add Done');
     res.status(200).send({
       success: true,
       data: req.body,
     });
->>>>>>> 7ebca862dfe9c20b0bfd86bbc932ca1c838e759f
   } catch (e) {
     res.status(404).send({
       success: false,
