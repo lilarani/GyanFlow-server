b772515bfaaf1b1f3479e4800b29334b3f87a934
Ankon Chowdhury <ankonchy1@gmail.com>
2025-05-01 19:21:15 +0600

    totalSale calculated in statsController

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -2,9 +2,60 @@
 // import Course from './../models/coursesMode.js';
 import Course from "../models/coursesMode.js";
 import User from "../models/userModel.js";
+import Payment from "../models/paymentModel.js";
 
 let courseStats = async (req, res) => {
   try {
+    // const result = await Payment.aggregate([
+    //   {
+    //     $group: {
+    //       _id: null,
+    //       totalSale: { $sum: "$courseId.price" },
+    //     },
+    //   },
+    // ]);
+
+    // const totalSale = result[0]?.totalSale || 0;
+    // console.log(totalSale)
+
+    const result = await Payment.aggregate([
+      // Match documents where courseId exists
+      {
+        $match: {
+          courseId: { $ne: null }
+        }
+      },
+      // Lookup to join with courses collection
+      {
+        $lookup: {
+          from: 'courses',
+          localField: 'courseId',
+          foreignField: '_id',
+          as: 'courseDetails'
+        }
+      },
+      // Unwind the courseDetails array
+      {
+        $unwind: '$courseDetails'
+      },
+      // Group to sum the price
+      {
+        $group: {
+          _id: null,
+          totalPrice: { $sum: '$courseDetails.price' }
+        }
+      },
+      // Project to clean up the output
+      {
+        $project: {
+          _id: 0,
+          totalPrice: 1
+        }
+      }
+    ]);
+
+    let total =  result.length > 0 ? result[0].totalPrice : 0;
+
     const totalCourse = await Course.countDocuments();
 
     const totalStudents = await User.countDocuments({ role: "student" });
@@ -12,7 +63,7 @@ let courseStats = async (req, res) => {
 
     res.status(200).send({
       success: true,
-      data: { totalCourse, totalStudents, totalInstructors },
+      data: { totalCourse, totalStudents, totalInstructors, total },
     });
   } catch (e) {
     res.status(404).send({
